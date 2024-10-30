eb48aa58b7ff0cd639f5ccc61b4256f8e4b7bba7
Noureldin17 <noureldinn.nm@gmail.com>
2024-10-30 14:03:31 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#E58D54', // Main accent color
+        lightOrange: '#FDECE2', // Secondary color
+        secondary: '#D7C5B5', // Secondary color
+        lightGray: '#f1f0f0', // Light gray for backgrounds
+        cream: '#faf5f0', // Cream for softer backgrounds
+        palePink: '#F3EEF1', // Light pink for subtle highlights
+      },
+    },
+  },
+  plugins: [],
+};
